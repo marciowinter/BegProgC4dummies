1260ff3645f116507e37f678e926cfe2d4be929e
Márcio Eduardo Moraes Winter <marcioemw@gmail.com>
2022-04-12 21:15:04 -0300

    Update ex0301.c

--- Chapter_03/ex0301.c
+++ Chapter_03/ex0301.c
@@ -1,1 +1,4 @@
-
+/*
+* Arquivo vazio utilizado como exemplo de execução e compilação sem haver instruções no arquivo de fonte.
+* Empty file used as an example of execution and compilation without instructions in the source file.
+*/
