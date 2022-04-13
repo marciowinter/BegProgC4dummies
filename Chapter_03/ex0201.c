7545b3dbdcbf5c964943c3097e765da809d9e636
Márcio Eduardo Moraes Winter <marcioemw@gmail.com>
2022-04-12 21:07:15 -0300

    Create ex0201.c

---
+++ Chapter_03/ex0201.c
@@ -0,0 +1,14 @@
+/*
+* Pt_BR: Arquivo básico do segundo capítulo, utilizado também no capítulo 3 como base para aprender a comentar um código.
+* Neste caso, trata-se de como criar um bloco de comentário.
+*
+* Eng: Basic file of the second chapter, also used in chapter 3 as a basis for learning to comment a code.
+* In ths case, this is how to create a comment block.
+*/
+
+#include <stdio.h>
+
+int main(){
+  puts("Greetings, human!"); // Oh, i almost forgot. This is a one line comment. :-) 
+  return 0;
+}
